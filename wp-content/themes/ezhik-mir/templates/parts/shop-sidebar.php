57d835d07ff41edd02c36f6186ea7fae55441928
olivoin <ss@p-motion.ru>
2016-11-20 21:21:29 +0400

    latest

--- wp-content/themes/ezhik-mir/templates/parts/shop-sidebar.php
+++ wp-content/themes/ezhik-mir/templates/parts/shop-sidebar.php
@@ -1,20 +1,8 @@
  <aside id="sidebar">
     <div class="sidebar-search">
-        <?php echo do_shortcode('[widget id="mxajaxsearchwidget-2"]'); ?>
+        <?php echo do_shortcode('[widget id="woocommerce_product_search-2"]'); ?>
     </div>
-    <div class="sidebar-instagram">
-        <?php echo do_shortcode('[widget id="null-instagram-feed-2"]'); ?>
-    </div>
-    <div class="sidebar-social">
-        <?php echo do_shortcode('[widget id="nav_menu-4"]'); ?>
-    </div>
-    <div class="sidebar-instagram-full">
-        <?php echo do_shortcode('[widget id="null-instagram-feed-3"]'); ?>
-    </div>
-    <div class="sidebar-tags">
-        <?php echo do_shortcode('[widget id="tag_cloud-2"]'); ?>
-    </div>
-    <div class="sidebar-popular">
-        <?php echo do_shortcode('[widget id="views-2"]'); ?>
+    <div class="sidebar-categories">
+        <?php echo do_shortcode('[widget id="woocommerce_product_categories-2"]'); ?>
     </div>
 </aside>
